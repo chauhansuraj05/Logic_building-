143cfc0236960cbc8d0d156db2af5123fdcd1053
chauhansuraj05 <chauhansuraj9805@gmail.com>
2026-09-25 15:30:58 +0530

    Fibonacci series Done

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int num = 10;
+        int a = 0, b = 1;
+        for (int i = 0; i <= num; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
